7858d4eedd4d708b49b7becde6f3b249eed0156f
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-04-15 07:21:19 +0200

    search

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ class App extends Component {
 
   // Search Github users
   searchUsers = async (text) => {
+    this.setState({
+      loading: true,
+    });
+
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: '',
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
   };
 
   onChange = (e) => {
